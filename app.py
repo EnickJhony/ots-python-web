c094bef5de7f8bb7910209890f25b4693764be3e
Enick Jhony <enick.jhony@gmail.com>
2023-09-05 01:09:03 +0000

    adicionando  e implantando no

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("meu App")
 
+posts = [
+    {
+        "titulo":"Primeira",
+        "texto":"Postagem nesse blog daora pa carai"
+    },
+    {
+        "titulo":"Segundo",
+        "texto":"Mais uma postagem nesse blog daora pa carai"
+    }
+]
+
 @app.route('/')
-def hello():
-    return "Hello Word"
+def exibir_entradas():
+    entradas = posts
+    return render_template("entradas.html", entradas=entradas)
